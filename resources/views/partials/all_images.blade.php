076ee5033c040e0dbafd939e26f2bc6f3b44f04c
bilawal khalid <ktkbilawal@gmail.com>
2018-12-03 15:22:38 +0500

    images view changed

--- resources/views/partials/all_images.blade.php
+++ resources/views/partials/all_images.blade.php
@@ -2,16 +2,16 @@
 @section('middle')
 <?php  $count=1; ?>
 
-<div style="margin-left: 2%; margin-right: 2%; min-height: 500px;">
+<div style="margin-left: 2%; margin-right: 2%; min-height: 500px; ">
 
     <h1 align="center"  style="margin-top: 2%;">Images</h1>
 
-    <div class="container">
+    <div class="container" style="width: 80%;">
         <div class="row">
             @foreach($images as $image)
 
                 <div class="col-md-3 col-sm-6 col-xs-6">
-                    <img style="width: 200px; height: 200px;" src="{{ URL::asset('public/uploads/media/') }}/{{ $image->file_name }}"><br>
+                    <img style="width: 200px; height: 200px; margin-top: 5%; text-align: center" src="{{ URL::asset('public/uploads/media/') }}/{{ $image->file_name }}"><br>
 <!--                    <span style="font-weight: bold; margin-left: 20%; font-size: 18px;" align="center">{{ $image->event_name }}</span>-->
                 </div>
 
--- resources/views/partials/members.blade.php
+++ resources/views/partials/members.blade.php
@@ -19,16 +19,19 @@
             <!--            <h2 class="my-4">Our Team</h2>-->
         </div>
         @foreach($members as $member)
-        <div class="col-lg-4 col-sm-6" style="text-align:center !important" >
-            <img class="" style="height: 200px; width: 200px; border-radius: 100px;" src="public/uploads/members/{{ $member->pic }}" alt="">
+
+        <div class="col-lg-4 col-sm-6" style="margin-top:5%;  text-align:center !important"  >
+            <img class="" style="height: 100px; width: 100px; border-radius: 100px;" src="public/uploads/members/{{ $member->pic }}" alt="">
 
             <h3 style="margin-top: 10px;"> {{ $member->name }}
 
             </h3>
-            <h5>Designation: {{ $member->designation }}</h5>
+            <h5>{{ $member->designation }}</h5>
             <h5>Contact Info: {{ $member->contact_no }}</h5>
+            <br>
 
         </div>
+        <br>
         @endforeach
 
     </div>
