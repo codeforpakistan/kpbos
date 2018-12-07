59f3bb66a740a8348010071b7e2dcf1dc6046a35
naveedali <naveedali9480@gmail.com>
2018-12-08 00:36:20 +0500

    analytics added

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -68,6 +68,16 @@
             color: white !important;
         }
     </style>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130639840-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-130639840-1');
+</script>
+
 </head>
 <body class="bg-pattern">
 
