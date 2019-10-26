f1a88040732730cc5fe27312db79a2bc1aeadb44
Mubassir Hayat <mubassirhayat@gmail.com>
2019-10-26 15:54:40 +0500

    fixing the issue with the publication by date page

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -65,8 +65,8 @@ class testController extends Controller
             'selectedYear'=>$selectedYear
             ]);
     }
-    public function publicationbydat($year,$id){
-        $allpublications= DB::Table('publication_uploads')->where('period',$year)->where('publication_id',$id)->paginate(5);
+    public function publicationbydat($selectedYear,$id){
+        $allpublications= DB::Table('publication_uploads')->where('period',$selectedYear)->where('publication_id',$id)->paginate(5);
         $years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030', '2031', '2032', '2033', '2034', '2035', '2036', '2037', '2038', '2039', '2040', '2041', '2042', '2043', '2044', '2045', '2046', '2047', '2048', '2049', '2050'];
         $publications=DB::Table('publications')->where('id',$id)->get();
         if(count($publications)>0){
