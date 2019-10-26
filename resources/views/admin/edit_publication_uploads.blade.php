827a61ceee0593af3759d003486439d8f795c357
Mubassir Hayat <mubassirhayat@gmail.com>
2019-10-26 13:40:20 +0500

    publications controller

--- resources/views/admin/edit_publication_uploads.blade.php
+++ resources/views/admin/edit_publication_uploads.blade.php
@@ -40,15 +40,9 @@
 
     <label>period</label>
     <select class="form-control" name="period">
-        <option value="2010">2010</option>
-        <option value="2011">2011</option>
-        <option value="2012">2012</option>
-        <option value="2013">2013</option>
-        <option value="2014">2014</option>
-        <option value="2015">2015</option>
-        <option value="2016">2016</option>
-        <option value="2017">2017</option>
-        <option value="2018">2018</option>
+        @foreach($years as $year)
+            <option value="{{ $year }}">{{ $year }}</option>
+        @endforeach
     </select><br>
 
     <label>Type</label>
