1c685df50c1914a045a3c7972291f74168c9cb97
Mubassir Hayat <mubassirhayat@gmail.com>
2019-10-26 13:26:32 +0500

    dropdown issue resolved

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -51,13 +51,19 @@ class testController extends Controller
     }
     public function allpublication($id){
         $allpublications= DB::Table('publication_uploads')->where('publication_id',$id)->paginate(8);
-
+        $years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030', '2031', '2032', '2033', '2034', '2035', '2036', '2037', '2038', '2039', '2040', '2041', '2042', '2043', '2044', '2045', '2046', '2047', '2048', '2049', '2050'];
          $publications=DB::Table('publications')->where('id',$id)->get();
          if(count($publications)>0){
              $name=$publications[0]->name;
          }
         $year="";
-        return view('partials.publications',array('allpublications'=>$allpublications,'id'=>$id,'name'=>$name,'year'=>$year));
+        return view('partials.publications',[
+            'allpublications'=>$allpublications,
+            'id'=>$id,
+            'name'=>$name,
+            'years' => $years,
+            'year'=>$year
+            ]);
     }
     public function publicationbydat($year,$id){
         $allpublications= DB::Table('publication_uploads')->where('period',$year)->where('publication_id',$id)->paginate(5);
